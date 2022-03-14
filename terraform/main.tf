2feced9ef6816ac9b9f34a8b293040a432bafcbf
mattklup <53016897+mattklup@users.noreply.github.com>
2022-03-14 23:09:48 +0000

    WIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,8 +19,8 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_virtual_network" "virtual_network" {
-    name                = "${var.resource_group_name.name}"
-    address_space       = ["10.0.0.0/24"]
-    location            = "westus"
-    resource_group_name = azurerm_resource_group.resource_group.name
+  name                = azurerm_resource_group.resource_group.name
+  address_space       = ["10.0.0.0/24"]
+  location            = "westus"
+  resource_group_name = azurerm_resource_group.resource_group.name
 }
