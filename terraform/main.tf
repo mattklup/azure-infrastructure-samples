9cdc42eaf7ababa30c3f9af9cb6ba16a283bbf5a
mattklup <53016897+mattklup@users.noreply.github.com>
2022-03-14 23:06:38 +0000

    WIP: vnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 2.91.0"
     }
   }
@@ -13,7 +13,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group_name}-${var.resource_group_location}"
   location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+    name                = "${var.resource_group_name}"
+    address_space       = ["10.0.0.0/24"]
+    location            = "westus"
+    resource_group_name = azurerm_resource_group.resource_group.name
 }
