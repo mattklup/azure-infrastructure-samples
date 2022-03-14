e78e871084395dd196d7c2f3ab0dc5a30b8f48fe
mattklup <53016897+mattklup@users.noreply.github.com>
2022-03-14 23:21:56 +0000

    WIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = "${var.resource_group_name}"
+  name     = var.resource_group_name
   location = var.resource_group_location
 }
 
